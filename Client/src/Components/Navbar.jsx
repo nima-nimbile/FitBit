e66cb9563da1fd969e779bd3152aacd974bac6e0
Nima <nima.mohamed@hotmail.com>
2023-06-20 10:29:59 -0700

    final push

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             <h3>About Me:</h3>
             <p className="description">My name is Nima. I graduated from Lighthouse Labs' Bootcamp in April 2023, where I honed my skills as a FullStack developer. Throughout the program, I gained expertise in a wide range of technologies, including React, CSS, HTML, Node.js, PostgreSQL, JavaScript, and Express. As a passionate junior developer, I am now eager to apply my knowledge and contribute to exciting projects in the field.</p>
             <h3>Website Overview:</h3>
-            <p className="description">Fitfolio is a cutting-edge website designed to support Bootcamp attendees or similar programs in their fitness journeys. My primary goal is to help you avoid unnecessary weight gain while you focus on your intensive training program. With the power of the OpenAI API, I provide accurate and up-to-date nutrition information to guide you in making informed dietary choices.</p>
+            <p className="description">Fitfolio is a cutting-edge website designed to support Bootcamp attendees or similar programs in their fitness journeys. My primary goal is to help you avoid unnecessary weight gain while you focus on your intensive training program. With the power of the OpenAI API, I provide accurate and up-to-date nutrition information to guide you in making dietary choices.</p>
           </div>
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/nima-mohamed/" target="_blank" rel="noopener noreferrer">
