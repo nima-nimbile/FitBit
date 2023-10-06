3cb824cb3205d9618a106f4a0fd7fdd4e6c9cadf
Nima <nima.mohamed@hotmail.com>
2023-10-06 11:22:59 -0700

    Add my resume and add icon on the title

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -4,6 +4,7 @@ import logo from "/Public/Logo-1.jpg"
 import linkedin from "/Public/linkedin-icon.png"
 import GitHub from "/Public/github-icon.png"
 import profile from "/Public/profile-picture.jpg"
+import Resume from "/Public/resume.png"
 
 
 
@@ -26,19 +27,22 @@ const Navbar = () => {
       </a>
       {isOpen && (
         <div className="menu-content">
-          <div className="profile">
+          <dl className="profile">
             <img className="profile-picture" src={profile} alt="Profile" />
-            <h3>About Me:</h3>
-            <p className="description">My name is Nima. I graduated from Lighthouse Labs' Bootcamp in April 2023, where I honed my skills as a FullStack developer. Throughout the program, I gained expertise in a wide range of technologies, including React, CSS, HTML, Node.js, PostgreSQL, JavaScript, and Express. As a passionate junior developer, I am now eager to apply my knowledge and contribute to exciting projects in the field.</p>
-            <h3>Website Overview:</h3>
-            <p className="description">Fitfolio is a cutting-edge website designed to support Bootcamp attendees or similar programs in their fitness journeys. My primary goal is to help you avoid unnecessary weight gain while you focus on your intensive training program. With the power of the OpenAI API, I provide accurate and up-to-date nutrition information to guide you in making dietary choices.</p>
-          </div>
+            <dt>About Me:</dt>
+            <dd className="description">My name is Nima. I graduated from Lighthouse Labs' Bootcamp in April 2023, where I honed my skills as a FullStack developer. Throughout the program, I gained expertise in a wide range of technologies, including React, CSS, HTML, Node.js, PostgreSQL, JavaScript, and Express. As a passionate junior developer, I am now eager to apply my knowledge and contribute to exciting projects in the field.</dd>
+            <dt>Website Overview:</dt>
+            <dd className="description">Fitfolio is a website designed to support Bootcamp attendees or similar programs. My primary goal is to help you avoid unnecessary weight gain while you focus on your intensive training program. With the power of the OpenAI API, I provide accurate and up-to-date nutrition information to guide you in making dietary choices.</dd>
+          </dl>
           <div className="social-icons">
             <a href="https://www.linkedin.com/in/nima-mohamed/" target="_blank" rel="noopener noreferrer">
               <img src={linkedin} alt="LinkedIn" />
             </a>
             <a href="https://github.com/nima-nimbile" target="_blank" rel="noopener noreferrer">
               <img src={GitHub} alt="GitHub" />
+            </a>
+            <a href="./Public/NimaMohamed.pdf" download>
+              <img src={Resume} alt="Resume" />
             </a>
           </div>
         </div>
